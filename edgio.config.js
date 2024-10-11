b2d4e0a5c61379c31fbf6622ecf864d11bb00f2b
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-10-11 09:38:30 +0800

    Update edgio.config.js

--- edgio.config.js
+++ edgio.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  connector: '@edgio/nodejs',
-}
+  connector: '@edgio/nodejs-connector',
+
+  nodejsConnector: {
+    // buildFolder: '.', // Your project does not contain a build output, setting this to the current directory will cause subdirectory copy error as seen above. Normally this will be ./dist, etc
+    entryFile: 'index.js',
+    envPort: 'PORT',
+    devCommand: 'node index.js', // instructs Edgio how to start your app server when running `edgio dev`
+  },
+
+  // Use the `origins` configuration for your upstream endpoints (https://docs.edg.io/applications/v7/performance/cdn_as_code/edgio_config#origins)
+  // backends: {
+    // The origin should not point to your local server
+    // origin: {
+    //   domainOrIp: 'localhost',
+    //   hostHeader: 'localhost',
+    // },
+  // },
+};
