ad409fd347cf8b11505cfc180948df30882c1dca
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2024-11-15 17:52:27 +0800

    Update edgio.config.js

--- edgio.config.js
+++ edgio.config.js
@@ -1,19 +1,14 @@
+// edgio.config.js
+const { join } = require('path');
+
 module.exports = {
-  connector: '@edgio/nodejs-connector',
-
-  nodejsConnector: {
-    // buildFolder: '.', // Your project does not contain a build output, setting this to the current directory will cause subdirectory copy error as seen above. Normally this will be ./dist, etc
-    entryFile: 'index.js',
-    envPort: 'PORT',
-    devCommand: 'node index.js', // instructs Edgio how to start your app server when running `edgio dev`
+  connector: '@edgio/express',
+  express: {
+    // 指定应用入口文件
+    appPath: join(process.cwd(), 'index.js'),
   },
-
-  // Use the `origins` configuration for your upstream endpoints (https://docs.edg.io/applications/v7/performance/cdn_as_code/edgio_config#origins)
-  // backends: {
-    // The origin should not point to your local server
-    // origin: {
-    //   domainOrIp: 'localhost',
-    //   hostHeader: 'localhost',
-    // },
-  // },
+  // 确保包含所有需要的文件
+  serverless: {
+    include: ['tmp/**/*'],
+  },
 };
